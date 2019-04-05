58201f75229fb74a6fe020a0daa7dc69344f8c02
erzan8 <48571176+erzan8@users.noreply.github.com>
2019-04-05 14:55:32 +0200

    Add files via upload

---
+++ Portfolio web/scripts/skills.js
@@ -0,0 +1,11 @@
+/*$(function() {
+    $('progress').each(function() {
+      $(this).animate({width: "100%"}, 500 );
+              });
+  });*/
+$(document).ready(function(){
+  $('.mono').click(function(){
+  $('nav').slideToggle().toggleClass('hide');
+})}
+);
+
